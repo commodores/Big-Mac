916ab3eaef608a323e850e3320e7149c065fc743
jmarkelford@gmail.com <jmarkelford@gmail.com>
2022-02-10 17:30:02 -0500

    Yes

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -5,19 +5,55 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 
 public class Climber extends SubsystemBase {
   
+  private final WPI_TalonFX ClimberElevate;
+  private final TalonSRX ClimberRotate;
+
   public Climber() {
 
+     ClimberElevate = new WPI_TalonFX(Constants.ClimberConstants.kClimberElevatePort);
+    ClimberElevate.configFactoryDefault();
+    ClimberElevate.setNeutralMode(NeutralMode.Brake);
+    ClimberElevate.set(ControlMode.PercentOutput, 0.0);
+  
+   ClimberRotate = new TalonSRX(Constants.ClimberConstants.kClimberRotatePort);
+    ClimberRotate.configFactoryDefault();
+    ClimberRotate.setNeutralMode(NeutralMode.Brake);
+    ClimberRotate.set(ControlMode.PercentOutput, 0.0);
+    
   }
 
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+  public void ClimberUp(double speed){
+    ClimberElevate.set(ControlMode.PercentOutput, speed);
+     ClimberRotate.set(ControlMode.PercentOutput, speed);
+
+  
+  }
+public void ClimberDown(double speed){
+    ClimberElevate.set(ControlMode.PercentOutput, speed);
+     ClimberRotate.set(ControlMode.PercentOutput, speed);
+     
+  
+  }
+
+  public void StopClimber(){
+    ClimberElevate.set(ControlMode.PercentOutput, 0.0);
+    ClimberRotate.set(ControlMode.PercentOutput, 0.0);
   }
 
 }
