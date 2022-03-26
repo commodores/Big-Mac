1bbadba4a080673d7ecb3f7a41a756038b907d07
commodore <darren.hagler@4cacademy.org>
2022-03-26 12:20:50 -0400

    flip

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -48,7 +48,7 @@ public class Climber extends SubsystemBase {
     climberRotate.configMotionCruiseVelocity(5000);
     climberRotate.configMotionAcceleration(5000);
 
-    climberSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 2);
+    climberSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 4);
 
     rotateLimitSwitch = new DigitalInput(1);
     elevateLimitSwitch = new DigitalInput(2);
